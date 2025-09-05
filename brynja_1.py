939e88bbdacd00defec2f31c15c044a505beaa5e
Tinna R <tinnarunarsd@gmail.com>
2025-09-05 11:30:05 +0000

    Update brynja_1.py

--- brynja_1.py
+++ brynja_1.py
@@ -1,9 +1,6 @@
 print("Hæ")
 
-
-<<<<<<< Updated upstream
-print("fly away babe") kaka
+print("fly away babe") #kaka
 #unnur er big boss
-=======
 print("Wasssup") and print("bleeeh")#unnur er big boss
->>>>>>> Stashed changes
+
